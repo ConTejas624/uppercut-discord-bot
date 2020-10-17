fc53e60a1f46d1f86f33727b1cde6b4b01b3a6a6
ConTejas624 <41122318+ConTejas624@users.noreply.github.com>
2020-10-17 16:26:04 -0400

    change the channel permissions to mute/unmute

--- among_us.py
+++ among_us.py
@@ -7,24 +7,22 @@ async def main(message, client):
         await message.channel.send("Error: join a voice channel in the server first")
         return
 
-    unmuted = message.author.voice.channel
+    v_channel = message.author.voice.channel
     server = message.guild
 
-    overwrites = {
-        server.default_role: discord.PermissionOverwrite(speak = False, deafen_members = True)
+    unmuted_overwrites = v_channel.overwrites_for(server.default_role)
+    muted_overwrites = {
+        server.default_role: discord.PermissionOverwrite(speak = False)
     }
 
-    muted_channel = await server.create_voice_channel('muted_channel', overwrites=overwrites)
     await message.channel.send("Among Us mode initialized")
 
 
     mode = message.content[13:] # would call screenshots.main() once have that # 'silence' or 'talk'
     if mode == 'silence':
-        for user in unmuted.members:
-            await user.move_to(muted_channel)
+        await v_channel.set_permissions(server.default_role, overwrite=muted_overwrites)
     elif mode == 'talk':
-        for user in muted_channel.members:
-            await user.move_to(unmuted)
+        pass
     else:
         await message.channel.send('error')
         print(message.content[13:])
@@ -34,7 +32,6 @@ async def main(message, client):
         if message.author.bot: # does not respond to commands from a bot
             return
         if m.content == '$among end':
-            for user in muted_channel.members:
-                await user.move_to(muted_channel)
+            pass
             await m.channel.send('Among Us mode exited')
             return
