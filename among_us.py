8994783b55089230b28870fe272e17a224c77d16
ConTejas624 <41122318+ConTejas624@users.noreply.github.com>
2020-10-17 18:19:13 -0400

    run every 1 seconds complete: cannot call stop()

--- among_us.py
+++ among_us.py
@@ -1,8 +1,10 @@
-import discord
+import discord, time, threading
 
 # runs Among Us mode code
 
-async def main(message, client):
+running = True
+
+async def main(message):
     if message.author.voice is None:    # Checks that the user who called the command is in a voice channel
         await message.channel.send("Error: join a voice channel in the server first")
         return
@@ -10,28 +12,25 @@ async def main(message, client):
     v_channel = message.author.voice.channel
     server = message.guild
 
-    unmuted_overwrites = v_channel.overwrites_for(server.default_role)
-    muted_overwrites = {
-        server.default_role: discord.PermissionOverwrite(speak = False)
-    }
-
     await message.channel.send("Among Us mode initialized")
 
 
-    mode = message.content[13:] # would call screenshots.main() once have that # 'silence' or 'talk'
-    if mode == 'silence':
-        await v_channel.set_permissions(server.default_role, overwrite=muted_overwrites)
-    elif mode == 'talk':
-        pass
-    else:
-        await message.channel.send('error')
-        print(message.content[13:])
+    myThread = threading.Timer(1, set_mode)
+    myThread.start(message, v_channel, server)
+    t = time.time()
+    while running == True:
+        await message.channel.send('clock')
 
-    @client.event
-    async def on_message(m):
-        if message.author.bot: # does not respond to commands from a bot
-            return
-        if m.content == '$among end':
-            pass
-            await m.channel.send('Among Us mode exited')
-            return
+        last_mode = 'first run'
+        mode = message.content[13:] # would call screenshots.main() once have that # 'silence' or 'talk'
+        if mode == 'silence' and last_mode != mode:
+            await v_channel.set_permissions(server.default_role, speak = False)
+        elif mode == 'talk' and last_mode != mode:
+            await v_channel.set_permissions(server.default_role, overwrite = None)
+        last_mode = mode
+        time.sleep(1-time.monotonic()%1)
+    
+    await v_channel.set_permissions(server.default_role, overwrite = None)
+
+async def stop():
+    running = False
