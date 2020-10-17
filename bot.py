42dd12514207aeb3efcfbe9a4b58dfd06213baf5
ConTejas624 <41122318+ConTejas624@users.noreply.github.com>
2020-10-17 11:27:51 -0400

    basic bot code

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+
+# discord.py API reference: https://discordpy.readthedocs.io/en/latest/api.html#
+# discord dev app portal: https://discord.com/developers/applications
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('logged in as {0.user}'.format(client))
+
+async def on_message():
+    if message.author == client.user: # makes sure the bot does not trigger itself
+        return
+    
+    if message.content.startswith('$ping'):
+        await message.channel.send('pong')
+
+token_file = open("resources\\token.txt", "r")
+token = file.read()
+token_file.close()
+
+client.run(token)
